1e61cc9634cd0655f40be93e53c06e9f0e3d7a21
Shreya Gawade <shreyagawade0405@gmail.com>
2024-06-19 13:31:44 +0530

    first commit

---
+++ emp_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import department,role,employee
+# Register your models here.
+admin.site.register(department)
+admin.site.register(role)
+admin.site.register(employee)
+
+
